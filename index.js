b9b28077d63ae5e9d7ca0f9691ff6c96e92e94ac
Antonin Archer <antoarcher@gmail.com>
2017-03-25 17:23:47 +0100

    Hello Nouvelle Ecole

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const express = require('express')
+  const express = require('express')
 const app = express()
+const router = express.Router();
+const bodyParser = require('body-parser');
+const template = require('pug');
+const path = __dirname + '/views/';
 
-app.use(express.static('public'))
+app.set('view engine', 'pug');
+app.set('views', './views')
 
-app.get('/', function (req, res) {
-  res.send('Bienvenue sur Nouvelle École')
-})
+
+// Fire database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nouvelleecole');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Successfully connected to database');
+});
+
+// parse request bodies (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.render('index');
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.pug");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000!')
 })
